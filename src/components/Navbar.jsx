8807869f862eef77970e0dfd38b2bb950bed5ffe
Natnael Abebe <natnaelabebe1994@gmail.com>
2024-04-10 11:54:13 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,20 @@ import React from "react";
 function Navbar() {
   return (
     <div className="">
-      <div className="">Pomodoro</div>
-      <div className="">short break</div>
-      <div className="">long break</div>
+      <h1 className="text-[#D6DCFA] text-3xl font-bold font-sans mb-6 p-8 w-full text-center">
+        pomodoro
+      </h1>
+      <div className="flex gap-x-4 justify-between w-[100%] mx-auto  rounded-full bg-[#141832] p-2 ">
+        <div className="text-[#646985] font-bold bg-[#F97170] p-3 rounded-full">
+          Pomodoro
+        </div>
+        <div className="text-[#646985] font-bold p-3 rounded-full">
+          short break
+        </div>
+        <div className="text-[#646985] font-bold p-3 rounded-full">
+          long break
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/pages/SharedLayout.jsx
+++ src/components/pages/SharedLayout.jsx
@@ -6,10 +6,9 @@ import { FaGear } from "react-icons/fa6";
 function SharedLayout() {
   return (
     <div className="bg-[#1F2141] h-dvh ">
-      <h1 className>Pomodoro</h1>
       <Navbar />
       <Outlet />
-      <FaGear className="text-[#78909F]" />
+      <FaGear className="text-[#78909F] " />
     </div>
   );
 }
@@ -17,6 +16,5 @@ function SharedLayout() {
 export default SharedLayout;
 
 // #D6DCFA Big Pomodoro  color
-// #646985 nav color
 // #F97170 the orange
 // #D7E0FF the number
